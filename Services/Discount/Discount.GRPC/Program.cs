2d2fa9b21b9ed18e630d786139ba73f428261eb9
SajaD RaaD <s.ebrahimiraad@gmail.com>
2023-02-08 02:01:28 +0330

    prepare discount.grpc (entities , repo , extension , ...)

--- Services/Discount/Discount.GRPC/Program.cs
+++ Services/Discount/Discount.GRPC/Program.cs
@@ -1,6 +1,10 @@
+using Discount.GRPC.Extensions;
+using Discount.GRPC.Repositories;
 using Discount.GRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
@@ -11,6 +15,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MigratePostgreDatabase();
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
