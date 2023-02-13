d4b47db265a202cf9366131f3306aefe5daf865d
SajaD RaaD <s.ebrahimiraad@gmail.com>
2023-02-14 02:57:27 +0330

    add custom exception to ordering.application layer

--- Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
+++ Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
 using Ordering.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -30,8 +31,7 @@ namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
 
             if (orderToDelete == null)
             {
-                _logger.LogError($"Order {request.Id} does not exist on database");
-                //throw new NotFoundException(nameof(Order), request.id);
+                throw new NotFoundException(nameof(Order), request.Id);
             }
 
             await _repository.DeleteAsync(orderToDelete);
